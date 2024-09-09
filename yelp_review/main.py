45e0b034434eb0c9406885e5acebcf8834080951
Mahadev Gaonkar <mahadevgaonkar@Mahadevs-MacBook-Pro.local>
2024-09-09 00:25:33 -0700

    yelp review training code added

--- yelp_review/main.py
+++ yelp_review/main.py
@@ -1,7 +1,13 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from transformers import (
+    AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments,
+    DataCollatorWithPadding
+)
 import evaluate
 import numpy as np
+
+NUM_TRAIN_SAMPLES = 100
+NUM_TEST_SAMPLES = 100
 
 def load_dataset_from_hf(dataset_name):
     dataset = load_dataset(dataset_name)
@@ -15,10 +21,31 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-cased")
 
     def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True)
+        return tokenizer(examples["text"], truncation=True)
     
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
-    model = AutoModelForSequenceClassification.from_pretrained("google-bert/bert-base-cased", num_labels=5)
+    train_dataset = dataset['train'].shuffle(seed=42).select(range(NUM_TRAIN_SAMPLES))
+    test_dataset = dataset['test'].shuffle(seed=42).select(range(NUM_TEST_SAMPLES)) 
+
+    # Free up memory
+    dataset = None
+
+    train_dataset_tokenized = train_dataset.map(tokenize_function, batched=True)
+    test_dataset_tokenized = test_dataset.map(tokenize_function, batched=True)
+
+    train_dataset_tokenized.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+    test_dataset_tokenized.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+
+    id2label = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5"}
+    label2id = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5}
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "google-bert/bert-base-cased", 
+        num_labels=5,
+        id2label=id2label,
+        label2id=label2id
+        )
     model.to("mps")
 
     def compute_metrics(pred):
@@ -26,30 +53,30 @@ def main():
 
         logits, labels = pred
         predictions = np.argmax(logits, axis=-1)
-        return metric.compute(predictions=predictions, references=labels)
+        accuracy = metric.compute(predictions=predictions, references=labels)
+        print(accuracy)
+        return accuracy
     
     
     training_args = TrainingArguments(output_dir="test_trainer", 
                                       eval_strategy="epoch", 
                                       per_device_train_batch_size=8, 
                                       per_device_eval_batch_size=8, 
-                                      num_train_epochs=3, 
+                                      num_train_epochs=5, 
                                       logging_dir="test_trainer/logs", 
                                       logging_steps=10, 
                                       eval_steps=10, 
-                                    #   load_best_model_at_end=True, 
-                                      metric_for_best_model="accuracy", 
-                                      greater_is_better=True)
+                                    #   learning_rate=1e-5,
+                                      metric_for_best_model="accuracy",
+                                      )
     
-    train = tokenized_datasets["train"]
-    test = tokenized_datasets["test"]
-
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train,
-        eval_dataset=test,
+        train_dataset=train_dataset_tokenized,
+        eval_dataset=test_dataset_tokenized,
         compute_metrics=compute_metrics,
+        data_collator=data_collator,
     )
 
     trainer.train()
